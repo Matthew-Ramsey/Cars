1b4090fc5e2ed66ff908bd27027c27c37f120005
Matthew-Ramsey <Matthew-ramsey@outlook.com>
2024-02-12 13:18:42 +0000

    Second lot of problems
    Ensuring that the version is java 17

--- src/main/java/me/ramsey/cars/CarsApplication.java
+++ src/main/java/me/ramsey/cars/CarsApplication.java
@@ -4,12 +4,14 @@ import me.ramsey.cars.objects.Engine;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Scanner;
 
 
 
+@RestController
 @SpringBootApplication
 public class CarsApplication {
 
